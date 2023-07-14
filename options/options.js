cb2480949e17e6fc65cd4b872ea28d9bc53d053e
Crowdbotics <team@crowdbotics.com>
2023-07-14 06:54:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mihir-mal-42194",
+  url: "https://mihir-mal-42194.botics.co",
+  api: "https://mihir-mal-42194.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
